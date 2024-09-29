fa5b8e86d779a089a6272a59e569a95716b43ecf
revenue-official <akunku5521@gmail.com>
2024-09-29 08:36:51 +0700

    dicoding "frontend web developer" final project

---
+++ src/script.js
@@ -0,0 +1,9 @@
+const header = document.querySelector('header')
+
+window.addEventListener('scroll', () => {
+	if (window.scrollY > 0) {
+		header.classList.add('scroll')
+	} else {
+		header.classList.remove('scroll')
+	}
+})
