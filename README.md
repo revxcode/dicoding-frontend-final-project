3c1c1babed7304d5012185037d2e7ea501b83c82
revenue-official <akunku5521@gmail.com>
2024-09-30 15:55:11 +0700

    Auto commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## Final Project Dicoding - Learn Front-End Web Development
+
+### Description
+This website is the final assignment of the [Learn Front-End Web Development](https://www.dicoding.com/academies/123/tutorials/4350) class at Dicoding. This website is made with HTML, CSS, and JavaScript. This website contains information about me, my portfolio, and my contact.
+
+### Features
+- Responsive Design
+- Contact Form
+- Animated Scroll
+
+### Instalasi
+1. Clone repository
+```
+git clone https://github.com/revxcode/dicoding-frontend-final-project.git
+```
+2. Buka file `index.html` di browser
+
+### Demo
+[Demo Website](https://revxcode.github.io/dicoding-frontend-final-project/)
--- src/script.js
+++ src/script.js
@@ -1,9 +1,19 @@
-const header = document.querySelector('header')
+const personalImageIcon = document.querySelectorAll('.personal-image-icon svg')
 
-window.addEventListener('scroll', () => {
-	if (window.scrollY > 0) {
-		header.classList.add('scroll')
-	} else {
-		header.classList.remove('scroll')
-	}
-})
+const automaticSlide = () => {
+  setInterval(() => {
+    personalImageIcon.forEach((icon, index) => {
+      // Get the current active class (e.g., 'active-1')
+      let activeClass = icon.classList.value.split('-')[1] 
+
+      // Calculate the new active class based on the index
+      let newActiveClass = 'active-' + ((parseInt(activeClass) % 3) + 1)
+
+      // Remove the old active class and add the new one
+      icon.classList.remove(`active-${activeClass}`) 
+      icon.classList.add(newActiveClass)
+    })
+  }, 3000)
+}
+
+automaticSlide()
